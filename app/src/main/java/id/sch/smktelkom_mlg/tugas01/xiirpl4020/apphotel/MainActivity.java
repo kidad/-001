97c78e39a674ab34460bef007bf322abd6d7952b
kidad <kidad.blythe@gmail.com>
2016-10-15 17:23:30 +0700

    edittext

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4020/apphotel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4020/apphotel/MainActivity.java
@@ -2,12 +2,45 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl4020.apphotel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama, etKtp;
+    Button btHasil;
+    TextView tvHasil;
+    CheckBox cbEB, cbS, cbL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etKtp = (EditText) findViewById(R.id.editTextKTP);
+        btHasil = (Button) findViewById(R.id.buttonHasil);
+        tvHasil = (TextView) findViewById(R.id.textView);
+        cbEB = (CheckBox) findViewById(R.id.checkBoxEB);
+        cbS = (CheckBox) findViewById(R.id.checkBoxS);
+        cbL = (CheckBox) findViewById(R.id.checkBoxL);
+
+        findViewById(R.id.buttonHasil).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        String ktp = etKtp.getText().toString();
+
+        tvHasil.setText("Nama : " + nama +
+                "\nNomor KTP : " + ktp);
     }
 }
